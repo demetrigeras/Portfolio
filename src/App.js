915059c4da206b57680b76be5781a9cb6695b899
demetrigeras <demetrijgeras@gmail.com>
2023-05-25 10:19:16 -0400

    new resume

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import profilePic from './profilePic.jpeg';
-import Resume from './DemetriGerasResume.pdf';
+import Resume from './DemetriGerasCV2023.pdf';
 
 function App() {
 
